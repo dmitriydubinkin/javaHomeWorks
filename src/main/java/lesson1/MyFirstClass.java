f0f10b9c055d9bb1939e133d36b9bea7c1008379
dmitriydubinkin <dmitriy.wk@yandex.ru>
2021-03-20 23:54:14 +0300

    Задание 2. Создал переменные и инициализировал их значения

--- src/main/java/lesson1/MyFirstClass.java
+++ src/main/java/lesson1/MyFirstClass.java
@@ -2,6 +2,33 @@ package lesson1;
 
 public class MyFirstClass {
     public static void main(String[] args) {
+        // какой-либо код ошибки из 8 бит
+        byte errorCode = 0x00000001;
+        // небольшое отрицательное число
+        short smallNumber = -28777;
+        // например, идентификатор задачи
+        int taskId = 22345045;
+        // очень большое число
+        long veryLongNumber = 700000000000L;
+        // давление в кПа
+        float pressure = 104.45f;
+        // итоговая цена чего-либо
+        double totalPrice = 254078.77;
 
+        // символ F, записанный двумя способами
+        char simbol1 = 'F';
+        char simbol2 = '\u0046';
+
+        // ссылочная переменная, строка с сообщением
+        String message = "Hi, my name is Dmitriy";
+
+        // флаг состояния, выбрано что-то или нет
+        boolean isChecked = true;
+
+        // пусть будет не выбрано
+        isChecked = false;
+
+        // пример константы, ускорение свободного падения
+        final float GRAVITY_ACCELERATION = 9.8f;
     }
 }
