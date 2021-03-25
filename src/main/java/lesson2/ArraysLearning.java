e827f4d807f8496262c0e573e8309010e226da71
dmitriydubinkin <dmitriy.wk@yandex.ru>
2021-03-25 10:35:30 +0300

    Задание 1. Задал int массив из 0 и 1, инвертировал с помощью цикла

--- src/main/java/lesson2/ArraysLearning.java
+++ src/main/java/lesson2/ArraysLearning.java
@@ -1,7 +1,21 @@
 package lesson2;
 
+import java.util.Arrays;
+
 public class ArraysLearning {
     public static void main(String[] args) {
+        initIntArrayAndInvert();
+    }
 
+    private static void initIntArrayAndInvert() {
+        int[] binary = {0, 1, 1, 0, 0, 1, 0};
+
+        System.out.printf("%-20s %s%n", "Исходный", Arrays.toString(binary));
+
+        for (int i = 0; i < binary.length; i++) {
+            binary[i] = (binary[i] != 0) ? 0 : 1;
+        }
+
+        System.out.printf("%-20s %s%n", "Инвертированный", Arrays.toString(binary));
     }
 }
