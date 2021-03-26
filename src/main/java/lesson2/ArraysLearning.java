575e43f7b40e4ea3c52d4116c320e0f2cb1da5fe
dmitriydubinkin <dmitriy.wk@yandex.ru>
2021-03-27 00:53:18 +0300

    Задание 7. Сдвиг реализован с помощью алгоритма Дугласа Макилроя, условия задания соблюдены

--- src/main/java/lesson2/ArraysLearning.java
+++ src/main/java/lesson2/ArraysLearning.java
@@ -24,6 +24,13 @@ public class ArraysLearning {
         } else {
             System.out.println("В переданном массиве нет баланса");
         }
+
+        // Задание 7. Реализован алгоритм с переворотами, доп.массивы не применялись ;))
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+        System.out.printf("%-20s %s%n", "Исходный", Arrays.toString(numbers));
+        shiftArray(numbers, -2);
+        System.out.printf("%-20s %s%n", "После сдвига", Arrays.toString(numbers));
     }
 
     private static void initIntArrayAndInvert() {
@@ -124,5 +131,31 @@ public class ArraysLearning {
         }
 
         return false;
+    }
+
+    private static void shiftArray(int[] data, int n) {
+        int offset = Math.abs(n);
+        int endIndex = data.length - 1;
+
+        if (n > 0) {
+            reverse(data, 0, endIndex - offset);
+            reverse(data, endIndex - offset + 1, endIndex);
+        } else {
+            // при левом сдвиге нагляднее видно что к чему
+            reverse(data, 0, offset - 1);
+            reverse(data, offset, endIndex);
+        }
+
+        reverse(data, 0, endIndex);
+    }
+
+    private static void reverse(int[] array, int start, int end) {
+        int koif = (int) Math.ceil((end - start) / 2f);
+
+        for (int i = 0; i < koif; i++) {
+            int tmp = array[start + i];
+            array[start + i] = array[end - i];
+            array[end - i] = tmp;
+        }
     }
 }
