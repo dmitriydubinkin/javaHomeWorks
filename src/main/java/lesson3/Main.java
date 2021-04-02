ececd3ae402ce06e3e63f655297443a4a2b3427c
dmitriydubinkin <dmitriy.wk@yandex.ru>
2021-04-02 20:39:52 +0300

    Подготовил каркас приложения

--- src/main/java/lesson3/Main.java
+++ src/main/java/lesson3/Main.java
@@ -1,7 +1,35 @@
 package lesson3;
 
-public class Main {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Main {
+    static final int RANGE_MAX = 20;
+    static final Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        runApplication();
+    }
+
+    private static void runApplication() {
+        // выводим приветственное сообщение
+        printHelloMessage();
+        // запускаем игру
+        playGame();
+        // завершаем работу приложения
+        stopApplication();
+    }
+
+    private static void printHelloMessage() {
+        System.out.println("Привет дружище, тебе предстоит угадать число!");
+        System.out.println("Вот подсказка, оно в диапазоне от 0 до " + RANGE_MAX);
+    }
+
+    private static void playGame() {
+        // TODO: реализовать саму игру
+    }
+
+    private static void stopApplication() {
+        input.close();
+        System.exit(0);
     }
 }
